adc36dfcc031d51bda4fe8b76d9c83f7cc47f45f
Vanessa <helena078santana@gmail.com>
2026-06-10 08:18:35 -0300

    Initial commit

--- 12.react-ts/src/App.tsx
+++ 12.react-ts/src/App.tsx
@@ -1,122 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./App.css";
+import BarraFerramentas from "./components/BotaoFerramentas";
+import Contador from "./components/Contador";
+import ContadorIntervalo from "./components/ContadorIntervalo";
+import Counter from "./components/Counter";
+import Exercicios from "./components/Exercicios";
+import ExibirPostagens from "./components/ExibirPostagem";
+import Greeting from "./components/Greeting";
+import TextInput from "./components/TextInput";
+import { TemaProvider } from "./context/TemaContext";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+      {/* 1 - Componentes funcionais com TS */}
+      <Greeting name="Matheus" />
+      {/* 2 - Hooks com tipos */}
+      <Counter />
+      {/* 3 - Manipulação de eventos com tipos */}
+      <TextInput />
+      {/* 4 - Custom hook com TS */}
+      <ContadorIntervalo />
+      {/* 5 - Exercícios */}
+      <Exercicios />
+      {/* 6 - useReducer e TypeScript */}
+      <Contador />
+      {/* 7 - Context API com TypeScript  */}
+      <TemaProvider>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+          <BarraFerramentas />
+          {/* Outros componentes que consomem o contexto TemaContext também viriam aqui */}
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      </TemaProvider>
+      {/* 8 - Axios com TS */}
+      <ExibirPostagens />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
